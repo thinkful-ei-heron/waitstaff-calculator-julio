25caad1c66645bf9f7c41cb05853601434af873f
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-24 14:01:51 -0500

    added all html and store

---
+++ script.js
@@ -0,0 +1,12 @@
+const store = {
+    baseMealPrice: 0.00,
+    taxRate: 0.00,
+    tipPercentage: 0.00,
+    subTotal: 0.00,
+    tip: 0.00,
+    total: 0.00,
+    tipTotal: 0.00,
+    mealCount: 0,
+    avgTipPerMeal: 0.00
+}
+
